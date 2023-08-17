085eadd196bb1d05f286e72ee8c788e8edc05387
Pankaj Borase <57318502+smartborse@users.noreply.github.com>
2023-08-17 13:28:57 +0000

    some changes in code

--- 01_Basic/01_var.js
+++ 01_Basic/01_var.js
@@ -1,1 +1,8 @@
-console.log("Pankaj")
+const accountId = 1232
+let emailId = "pankaj@google.com"
+var accountPassword = "1234"
+accountCity = "Jalgaon"
+console.log(accountId)
+console.log([accountId, emailId,accountPassword,accountCity])
+
+ 
