a7ab45e56c6c9ef084c5386d8ac7c5e2ca8cb85f
Pankaj Borase <57318502+smartborse@users.noreply.github.com>
2023-08-18 03:21:20 +0000

    learn about datatypes

---
+++ 01_Basic/02_datatypes.js
@@ -0,0 +1,11 @@
+let state = "Pankaj"    //string
+// let changeStrToNum=Number(state)
+var isLoggedIn = true;  //boolean
+let evenNumber = 2;     //number
+let form = null         //null
+let logIn               //undefined
+let bigInt = 220209393933993;   //bigint => large value in number
+let symbol                      //symbol => is a unique
+console.log(bigInt);/* 
+console.log(state);
+console.log(typeof isLoggedIn); */
