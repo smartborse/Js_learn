cafb6fc8f70c35fdcdecbe6224c6dbe059227a65
Pankaj Borase <57318502+smartborse@users.noreply.github.com>
2023-08-23 03:43:25 +0000

    learn conversion_comparisons

---
+++ 01_Basic/03_strToNumberConvt.js
@@ -0,0 +1,10 @@
+
+// ************* Operations ****
+
+let str= "pankaj"
+let str2=" Pankaj"
+let str3 = str+str2
+console.log(str3)
+
+console.log(( 3 + 4) * 5 % 3)
+   
---
+++ 01_Basic/04_comparisonDatatypes.js
@@ -0,0 +1,27 @@
+// console.log(2 > 1);     //return true or false
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 <= 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+
+// console.log("2" > 1)       // true
+// console.log("02" > 1)      // true
+
+console.log(null > 0)       //false
+console.log(null == 0)      //false 
+console.log(typeof(null >= 0))      //true //boolean -> typeof
+
+console.log(undefined > 0)        //false
+console.log(undefined == 0)       //false
+console.log(undefined >= 0)       //false
+
+// ===  => check even datatypes and value
+
+console.log("2" === 2)        //false
+console.log("2" == 2)         //true 
+console.log("2" === "2")      //true 
+console.log("2" === 2)        //true 
+
+
