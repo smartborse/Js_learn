cf4c9ab12e0592aac869bdb05d4ce116124981fe
Pankaj Borase <57318502+smartborse@users.noreply.github.com>
2023-08-30 12:14:03 +0000

    new topic array

---
+++ 02_Basic/01_Array.js
@@ -0,0 +1,38 @@
+const arr = [ 1, 2, 3, 4, 5, 6 ]
+console.log(`origional Array list : ${arr}`);
+
+const myHero = ["shaktiman", "Boom-boom", "marvel"]
+// console.log(myHero)
+/* 
+    //Print array element of index value: 
+console.log(arr[0]);    //0 index = 1
+console.log(arr[1]);    //1 index = 2
+console.log(arr[2]);    //2 index = 3
+console.log(arr[3]);    //3 index = 4
+console.log(arr[4]);    //4 index = 5
+console.log(arr[5]);    //5 index = 6           */
+
+
+const arr1 = new Array(10,20,30,'Smart',"Pankaj",'strawberry')
+// console.log(arr1);
+
+
+// Methods 
+arr.push(200)     // add a last index of arrry
+// console.log( arr)
+arr.push(890)
+// console.log( arr)
+
+
+arr.pop()
+// console.log(arr);   //remove last index element of array
+ 
+
+arr.unshift(12)     //inser a start eleemnt
+console.log(arr)
+
+arr.unshift(14)
+console.log(arr)
+
+
+
